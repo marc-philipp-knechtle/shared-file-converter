d59987c87ede0d819fd77a20561438855d682ab6
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2022-02-23 19:36:54 +0100

    read xml file cfile from file system

---
+++ converter/reader.py
@@ -0,0 +1,11 @@
+import xmltodict
+
+
+def read_xml(filepath: str) -> str:
+    with open(filepath, "r") as file:
+        content: str = file.read()
+        return content
+
+
+def read_and_convert_to_dict(filepath: str) -> dict:
+    return xmltodict.parse(read_xml(filepath), process_namespaces=True)
