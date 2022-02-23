41ec84b21dcfa48d7e4ad32060086a293e090dab
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2022-02-23 19:34:56 +0100

    create + activate venv in sert-development script

--- setup-development.sh
+++ setup-development.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+python3 -m venv .venv
+source .venv/bin/activate
+
 git submodule update --init --recursive
 
 DIR="shared-file-format"
@@ -9,7 +12,7 @@ if [ ! -d "$DIR" ]; then
   exit 1
 fi
 
-pip install -e shared-file-format/python
+pip install -r requirements.txt
 
 PIP_LIST=$(pip list)
 DOCRECSON='docrecjson'
@@ -17,5 +20,3 @@ if ! echo "$PIP_LIST" | grep -q "$DOCRECSON"; then
   echo "Error: ${DOCRECSON} seems not to be installed please verify this."
   exit 1
 fi
-
-pip install -r requirements.txt
