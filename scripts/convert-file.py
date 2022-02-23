82e63d3d4d7551983c3546bc27e371585b02f557
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2022-02-23 19:37:24 +0100

    change arguments fo use lowercase _

--- scripts/convert-file.py
+++ scripts/convert-file.py
@@ -1,34 +1,59 @@
 import argparse
+import sys
+import converter
+
 from argparse import Namespace
+
+from loguru import logger
+
+from converter import reader
+
+# remove the default loguru logger
+logger.remove()
+# add new custom loggers
+logger.add(sys.stdout, level='INFO')
+logger.add("errors.log", level='ERROR', rotation="1 MB")
 
 
 def parse_arguments() -> Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input-file", type=str, help="Please specify the file you want to convert.")
-    parser.add_argument("-o", "--output-file", type=str,
+    parser.add_argument("-i", "--input_file", type=str, help="Please specify the file you want to convert.")
+    parser.add_argument("-o", "--output_file", type=str,
                         help="Please specify the file you want the created json written into. "
                              "Please note that all convent of this file will be deleted. "
                              "This has to be specified if there is no mongo database connection specified.",
                         default=None)
-    parser.add_argument("-dbcon", "--db-connection", type=str,
+    parser.add_argument("-dbcon", "--db_connection", type=str,
                         help="Please specify this if you want to write the converted contents "
                              "into a mongo database instance. "
                              "This has to be specified if there is no output-file given."
                              "The connection string should look like this: "
                              "mongodb://user:password@ip:dbport/",
                         default=None)
-    parser.add_argument("-dbcol", "--db-collection", type=str,
+    parser.add_argument("-dbcol", "--db_collection", type=str,
                         help="This denotes the collection where the created json should be written into.", default=None)
     return parser.parse_args()
 
 
 def check_args(args: Namespace):
     # todo
-    pass
+    assert args.output_file is not None
 
 
 def main(args: Namespace):
     check_args(args)
+    input_filepath: str = args.input_file
+    output_filepath: str = args.output_file
+    db_connection: str = args.db_connection
+    db_collection: str = args.db_collection
+
+    # todo read file and convert to dict
+    content_to_dict: dict = reader.read_and_convert_to_dict(input_filepath)
+    logger.info("Started processing on file with name: [" + input_filepath + "]")
+
+    # todo process dict to shared-file-format
+
+    # todo write to file or database
 
 
 if __name__ == "__main__":
