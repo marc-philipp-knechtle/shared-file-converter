c99f349df4d55c8392d71619c0cbed32e6ae6c8c
Marc-Philipp Knechtle <mpknechtle@gmail.com>
2022-02-25 16:12:05 +0100

    move reader and add simple validation mechanism

---
+++ converter/validator/reader.py
@@ -0,0 +1,25 @@
+import xmltodict
+from loguru import logger
+from lxml import etree
+
+
+def read_xml(filepath: str) -> str:
+    with open(filepath, "r") as file:
+        content: str = file.read()
+        validation_result: bool = validate(filepath, "page-xml/2017-07-15.xsd")
+        if not validation_result:
+            logger.error("The read XML File does not match to the specified schema.")
+        return content
+
+
+def read_and_convert_to_dict(filepath: str) -> dict:
+    return xmltodict.parse(read_xml(filepath))
+
+
+def validate(xml_path: str, xsd_path: str) -> bool:
+    xmlschema_doc = etree.parse(xsd_path)
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+
+    xml_doc = etree.parse(xml_path)
+
+    return xmlschema.validate(xml_doc)
